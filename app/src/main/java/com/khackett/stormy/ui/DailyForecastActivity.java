7bcf003eaed1b49066174a145396057e16baf2a4
KevinH72 <kevinhackett72@gmail.com>
2015-07-04 21:03:00 +0100

    Day class is now parceable (read and write). Day objects can now be unparcelled in DailyForecastActivity.

--- app/src/main/java/com/khackett/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/com/khackett/stormy/ui/DailyForecastActivity.java
@@ -1,11 +1,15 @@
 package com.khackett.stormy.ui;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 
 import com.khackett.stormy.R;
 import com.khackett.stormy.adapters.DayAdapter;
 import com.khackett.stormy.weather.Day;
+
+import java.util.Arrays;
 
 public class DailyForecastActivity extends ListActivity {
 
@@ -16,8 +20,16 @@ public class DailyForecastActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_forecast);
 
-        DayAdapter adapter = new DayAdapter(this, mDays);
+        // get the intent that started this activity
+        Intent intent = getIntent();
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.DAILY_FORECAST);
 
+        // convert this into an array of Day objects, using code from the Day class
+        // use copyOf() method to copy one array into another - parcelables into Day[]
+        mDays = Arrays.copyOf(parcelables, parcelables.length, Day[].class);
+
+        DayAdapter adapter = new DayAdapter(this, mDays);
+        setListAdapter(adapter);
     }
 
 }
--- app/src/main/java/com/khackett/stormy/weather/Day.java
+++ app/src/main/java/com/khackett/stormy/weather/Day.java
@@ -1,5 +1,8 @@
 package com.khackett.stormy.weather;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
@@ -9,7 +12,7 @@ import java.util.TimeZone;
  * This is a model class in the MVC pattern
  * Created by KHackett on 04/07/15.
  */
-public class Day {
+public class Day implements Parcelable {
 
     private long mTime;
     private String mSummary;
@@ -71,4 +74,61 @@ public class Day {
         Date dateTime = new Date(mTime * 1000);
         return formatter.format(dateTime);
     }
+
+    /**
+     * Describe the kinds of special objects contained in this Parcelable's
+     * marshalled representation.
+     *
+     * @return a bitmask indicating the set of special object types marshalled
+     * by the Parcelable.
+     */
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    /**
+     * Flatten this object in to a Parcel.
+     *
+     * @param dest  The Parcel in which the object should be written.
+     * @param flags Additional flags about how the object should be written.
+     *              May be 0 or {@link #PARCELABLE_WRITE_RETURN_VALUE}.
+     */
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(mTime);
+        dest.writeString(mSummary);
+        dest.writeDouble(mTemperatureMax);
+        dest.writeString(mIcon);
+        dest.writeString(mTimeZone);
+    }
+
+    /**
+     * default constructor
+     */
+    public Day(){
+
+    }
+
+    // read data in the same order as it was written in writeToParcel()
+    // use a private constructor that takes a Parcel object as a parameter
+    private Day(Parcel in) {
+        mTime = in.readLong();
+        mSummary = in.readString();
+        mTemperatureMax = in.readDouble();
+        mIcon = in.readString();
+        mTimeZone = in.readString();
+    }
+
+    public static final Creator<Day> CREATOR = new Creator<Day>() {
+        @Override
+        public Day createFromParcel(Parcel source) {
+            return new Day(source);
+        }
+
+        @Override
+        public Day[] newArray(int size) {
+            return new Day[size];
+        }
+    };
 }
